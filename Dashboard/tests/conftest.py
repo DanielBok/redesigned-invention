eaad502b429e21611cba94a2ffdbe722f129f98c
Daniel Bok <daniel.bok@outlook.com>
2017-06-07 10:06:10 +0800

    Added basic test files

--- Dashboard/tests/conftest.py
+++ Dashboard/tests/conftest.py
@@ -1,22 +1,19 @@
-from os.path import abspath, dirname, join, exists
-import os
-
 import pytest
 
 from Dashboard.app import create_app
-from database_utils.setup import seed
-
-ROOT_FOLDER = abspath(join(dirname(__file__), '..'))
-TEST_DB_PATH = join(ROOT_FOLDER, 'app_data', 'data.db')
-TEST_DB_URI = "sqlite:///" + TEST_DB_PATH
+from Dashboard.blueprints.user.models import User
+from Dashboard.extensions import db as _db
+from configs.settings import SQLALCHEMY_DATABASE_URI
 
 
 @pytest.yield_fixture(scope='session')
 def app():
+    db_uri = SQLALCHEMY_DATABASE_URI.replace('data.db', 'data_test.db')
     params = {
         'DEBUG': False,
         'TESTING': True,
-        'SQLALCHEMY_DATABASE_URI': TEST_DB_URI
+        'WTF_CSRF_ENABLED': False,
+        'SQLALCHEMY_DATABASE_URI': db_uri
     }
 
     _app = create_app(settings_override=params)
@@ -33,14 +30,85 @@ def app():
 def client(app):
     """
     Setup an app client which gets passed to each test function
-    :param app: PyTest fixture
+    :param app: PyTest Fixture
     :return: Flask app client
     """
     yield app.test_client()
 
 
-# @pytest.fixture(scope='session')
-# def db(app, request):
-#     if not exists(TEST_DB_PATH):
-#         seed(create_app({'SQLALCHEMY_DATABASE_URI': TEST_DB_URI}))
+@pytest.fixture(scope='session')
+def db(app):
+    """
+    Setup database. This only gets executed once per session
+    :param app: PyTest Fixture
+    :return: SQLAlchemy database session
+    """
 
+    _db.drop_all()
+    _db.create_all()
+
+    user_seeds = [{
+        'role': 'manager',
+        'username': 'manager',
+        'password': 'test',
+        'name': 'Roger Federer'
+    }, {
+        'role': 'driver',
+        'username': 'driver',
+        'password': 'test',
+        'name': 'John Smith'
+    }]
+
+    for u in user_seeds:
+        _db.session.add(User(**u))
+
+    _db.session.commit()
+
+    return _db
+
+
+@pytest.yield_fixture(scope='function')
+def session(db):
+    """
+    Speeds up tests by using rollbacks and nested sessions. Requires that database support SQL savepoints. Postgres
+    does.
+
+    Read more at:
+    http://stackoverflow.com/a/26624146
+    :param db: Pytest Fixture
+    :return: None
+    """
+    db.session.begin_nested()
+
+    yield db.session
+
+    db.session.rollback()
+
+
+@pytest.fixture(scope='function')
+def users(db):
+    """
+    Create user fixtures. They reset per test
+    :param db: PyTest Fixture
+    :return: SQLAlchemy database session
+    """
+    db.session.query(User).delete()
+
+    user_seeds = [{
+        'role': 'manager',
+        'username': 'manager',
+        'password': 'test',
+        'name': 'Roger Federer'
+    }, {
+        'role': 'driver',
+        'username': 'driver',
+        'password': 'test',
+        'name': 'John Smith'
+    }]
+
+    for u in user_seeds:
+        db.session.add(User(**u))
+
+    db.session.commit()
+
+    return db
