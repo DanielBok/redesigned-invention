76e5b75e08a72045a495da88607cc8667f850939
Daniel Bok <daniel.bok@outlook.com>
2017-06-07 10:02:38 +0800

    Refactored configs/settings code

--- configs/settings.py
+++ configs/settings.py
@@ -1,29 +1,12 @@
-def _hidden_env_data():
-    import os
-    from os.path import dirname, join, abspath
+import os
+from os import getenv
+from os.path import abspath, dirname, join
 
-    grab = os.environ.get
-    root_folder = abspath(join(dirname(__file__), '..'))
-
-    data = {
-        'DEBUG': grab('DEBUG', True),
-        'DEBUG_TB_INTERCEPT_REDIRECTS': grab('DEBUG_TB_INTERCEPT_REDIRECTS', False),
-        'SECRET_KEY': grab('SECRET_KEY', os.urandom(128).hex()),
-        'SERVER_NAME': grab('SERVER_NAME', 'localhost:5000'),
-        'SQLALCHEMY_DATABASE_URI': grab('DATABASE_URL',
-                                        "sqlite:///{path}".format(path=join(root_folder, 'app_data', 'data.db'))),
-
-    }
-
-    return data
-
-
-_data = _hidden_env_data()
-
-DEBUG = _data['DEBUG']
-DEBUG_TB_INTERCEPT_REDIRECTS = _data['DEBUG_TB_INTERCEPT_REDIRECTS']
-SECRET_KEY = _data['SECRET_KEY']
+DEBUG = getenv('DEBUG', True)
+DEBUG_TB_INTERCEPT_REDIRECTS = getenv('DEBUG_TB_INTERCEPT_REDIRECTS', False)
+SECRET_KEY = getenv('SECRET_KEY', os.urandom(128).hex())
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = _data['SQLALCHEMY_DATABASE_URI']
+SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL',
+                                 "sqlite:///{0}".format(abspath(join(dirname(__file__), '..', 'app_data', 'data.db'))))
 TIMEZONE = 'Asia/Singapore'
-SERVER_NAME = _data['SERVER_NAME']
+SERVER_NAME = getenv('SERVER_NAME', 'localhost:5000')
