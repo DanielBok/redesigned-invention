9929edeab63895c267ceaf50509fd6f92c0fbb11
Daniel Bok <daniel.bok@outlook.com>
2017-06-13 11:49:58 +0800

    Added api/models test

--- Dashboard/tests/api/test_models.py
+++ Dashboard/tests/api/test_models.py
@@ -1,17 +1,22 @@
-from Dashboard.blueprints.api.models import Drivers
+from datetime import timedelta as td
+
+from Dashboard.blueprints.api.models import Tasks
+from Dashboard.tests.data_for_test import T_tasks
+from utils.datetime import now
 from utils.tests_helpers import TestMixin, AssertsMixin
 
 
 class TestModels(TestMixin, AssertsMixin):
-    def test_drivers_update(self):
+    def test_get_all_tasks_since(self):
+        start = now() + td(minutes=20)
+        end = now() + td(minutes=40)
 
-        d = Drivers.get_by_identity('Stan Mohan')
-        d.ready()
+        counts = 0
+        for t in T_tasks:
+            if start <= t['ready_time'] <= end:
+                counts += 1
 
-        assert d.status.code == 'on'
+        tasks_ = Tasks.get_all_tasks_since(start, end)
 
-        d.stop_work('break')
-        assert d.status.code == 'break'
+        assert len(tasks_) == counts
 
-        d.stop_work('stop')
-        assert d.status.code == 'off'
