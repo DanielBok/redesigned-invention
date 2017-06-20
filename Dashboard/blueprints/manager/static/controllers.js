d218af48af3467d26a9d9bbf0c3ead907c030d06
Daniel Bok <daniel.bok@outlook.com>
2017-06-20 18:08:22 +0800

    Made dt aware js

--- Dashboard/blueprints/manager/static/controllers.js
+++ Dashboard/blueprints/manager/static/controllers.js
@@ -26,8 +26,18 @@ app.controller('fsCtrl', ($scope, $http) => {
 
     $http.get(api('flights')).then(
         (res) => {
-            // console.log(res);
+
+            let time_now = new Date();
+            let tz_offset = -time_now.getTimezoneOffset() / 60;
+            tz_offset = tz_offset > 0 ? `+0${tz_offset}00` : `-0${tz_offset}00`;
+
             $scope.schedule = res.data.schedule;
+
+            res.data.schedule.forEach(e => {
+                e.scheduled_time = (new Date(`${e.scheduled_time}${tz_offset}`)).toGMTString();
+                e.actual_time = (new Date(`${e.actual_time}${tz_offset}`)).toGMTString();
+            });
+
             $scope.currentTerminal = true;
             $scope.fs.loading = false;
         },
@@ -38,22 +48,29 @@ app.controller('fsCtrl', ($scope, $http) => {
 });
 
 // Controller for taskboard
-app.controller('tbCtrl', ($scope, $http , $interval, $q) => {
+app.controller('tbCtrl', ($scope, $http, $interval, $q) => {
     $scope.loading = true;
     $scope.disableAllocateButton = true;
     $scope.editing = false;
 
+    let make_tasks_aware_tz = tasks => {
+        let time_now = new Date();
+        let tz_offset = -time_now.getTimezoneOffset() / 60;
+        tz_offset = tz_offset > 0 ? `+0${tz_offset}00` : `-0${tz_offset}00`;
+
+        tasks.forEach(e => {
+            e.flight_time = (new Date(`${e.flight_time}${tz_offset}`)).toGMTString();
+            e.ready_time = (new Date(`${e.ready_time}${tz_offset}`)).toGMTString();
+        })
+    };
+
     $scope.loadTaskBoard = () => {
 
-        // console.log("loading taskboard");
-        // Resolve all your promises simultaneously.
-        // Previous resolution was asynchronous and led to concurrency errors
-        $q.all([
-            $http.get(api('tasks', {type: 'all'})),
-        ]).then(data => {
-            let tasks = data[0].data;
+        $http.get(api('tasks', {type: 'all'})).then(res => {
+            let tasks = res.data;
             $scope.tasks = tasks.tasks;
-            // console.log("tasks: ", $scope.tasks)
+
+            make_tasks_aware_tz($scope.tasks);
             $scope.checkTimings();
 
         }, err => {
@@ -108,31 +125,31 @@ app.controller('tbCtrl', ($scope, $http , $interval, $q) => {
     };
 
     $scope.checkTimings = () => {
-        var time_now = new Date();
+        let time_now = new Date();
         let tz_offset = -time_now.getTimezoneOffset() / 60;
         tz_offset = tz_offset > 0 ? `+0${tz_offset}00` : `-0${tz_offset}00`;
-        // console.log('time now:', time_now);
-        $scope.tasks.forEach( task => {
-            var task_time = new Date(`${task.flight_time}${tz_offset}`);
-            var remaining = Math.round((task_time - time_now)/60000);
-            if (remaining < 0){
+
+        $scope.tasks.forEach(task => {
+            let task_time = new Date(`${task.flight_time}${tz_offset}`);
+            let remaining = Math.round((task_time - time_now) / 60000);
+            if (remaining < 0) {
                 task.flag = -1; // overdue task
             } else if (remaining < 20) {
                 task.flag = 1; // urgent task
             } else {
-              task.flag = 0; // normal task
-            };
+                task.flag = 0; // normal task
+            }
         })
     };
 
     $scope.loadTaskBoard(); // first load
 
-    $interval(function() {
-      // Call interval function only after first load
-    	if (!$scope.editing && !$scope.loading){
-    		$scope.loadTaskBoard();
-        Materialize.toast('Taskboard refreshed!', 2000)
-    	}
+    $interval(function () {
+        // Call interval function only after first load
+        if (!$scope.editing && !$scope.loading) {
+            $scope.loadTaskBoard();
+            Materialize.toast('Taskboard refreshed!', 2000)
+        }
     }, 20000);
 
 });
